cdff4852a8ba343fcd43dc94b289338deb5fa58f
NAO5802 <i100185b@gmail.com>
2026-03-23 20:22:20 +0900

    Add test for createUser

--- src/test/kotlin/com/example/userapi/service/UserServiceTest.kt
+++ src/test/kotlin/com/example/userapi/service/UserServiceTest.kt
@@ -1,12 +1,14 @@
 package com.example.userapi.service
 
 import com.example.userapi.exception.UserNotFoundException
+import com.example.userapi.model.CreateUserRequest
 import com.example.userapi.model.User
 import com.example.userapi.repository.UserEntity
 import com.example.userapi.repository.UserRepository
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 import java.util.Optional
@@ -18,8 +20,11 @@ class UserServiceTest {
 
     private val entityOfUser1 = UserEntity(1L, "Alice", "alice@example.com")
     private val entityOfUser2 = UserEntity(2L, "Bob", "bob@example.com")
+    private val entityOfUser3 = UserEntity(3L, "Cathy", "cathy@example.com")
     private val user1 = User(1L, "Alice", "alice@example.com")
     private val user2 = User(2L, "Bob", "bob@example.com")
+    private val newUserRequest = CreateUserRequest("Cathy", "cathy@example.com")
+    private val existingUserRequest = CreateUserRequest("Alice", "alice@example.com")
 
     @Test
     fun `getAllUsers_ユーザーが存在する時、全てのユーザーを返す`() {
@@ -57,7 +62,22 @@ class UserServiceTest {
     }
 
     @Test
-    fun createUser() {
+    fun `createUser_正しいリクエストの場合、作成したユーザーを返す`() {
+        whenever(userRepository.findByEmail(newUserRequest.email)).thenReturn(null)
+        // NOTE: Mockioは引数をequalsで照合する。class(非data class)のequalsは参照比較のため別インスタンスはマッチしない。テストではany()で回避している
+        whenever(userRepository.save(any())).thenReturn(entityOfUser3)
+
+        val actual = userService.createUser(newUserRequest)
+
+        assertEquals("Cathy", actual.name)
+        assertEquals("cathy@example.com", actual.email)
+    }
+
+    @Test
+    fun `createUser_重複したEmalをリクエストした場合、例外を返す`() {
+        whenever(userRepository.findByEmail(existingUserRequest.email)).thenReturn(entityOfUser1)
+
+        assertThrows<IllegalArgumentException> { userService.createUser(existingUserRequest) }
     }
 
 }
