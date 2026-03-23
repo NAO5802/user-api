6567131fbd22b4a3425bb9f554b3d601444818ae
NAO5802 <i100185b@gmail.com>
2026-03-23 13:36:44 +0900

    remove unnecessary space

--- src/main/kotlin/com/example/userapi/repository/TaskRepository.kt
+++ src/main/kotlin/com/example/userapi/repository/TaskRepository.kt
@@ -10,7 +10,7 @@ interface TaskRepository: JpaRepository<TaskEntity, Long>{
         SELECT t FROM TaskEntity t 
         WHERE t.userId = :userId
         AND (:title IS NULL OR t.title LIKE %:title%)
-        AND (:status IS NULL OR t.status = : status)
+        AND (:status IS NULL OR t.status = :status)
     """)
     fun findAllWithFilter(@Param("userId") userId: Long, @Param("title") title: String?,@Param("status") status: TaskStatus?): List<TaskEntity>
 }
