c88c88fe71d4d3fd0e3b5e4e58dcd67cf89b137f
NAO5802 <i100185b@gmail.com>
2026-04-13 15:31:56 +0900

    refactor sortFromString

--- src/main/kotlin/com/example/userapi/util/Sort.kt
+++ src/main/kotlin/com/example/userapi/util/Sort.kt
@@ -4,8 +4,8 @@ import com.example.userapi.model.TaskSortKey
 import org.springframework.data.domain.Sort
 
 fun sortFromString(sortBy: String, sortDir: String): Sort {
-    val sortBy = sortBy.let { TaskSortKey.fromString(it) }
-    val sortDir = sortDir.let{ Sort.Direction.fromString(it) }
+    val key = TaskSortKey.fromString(sortBy)
+    val direction = Sort.Direction.fromString(sortDir)
 
-    return Sort.by(Sort.Order(sortDir, sortBy.label))
+    return Sort.by(Sort.Order(direction, key.label))
 }
